171191e7b64d9c4bc39aac0f0c14e4dda32456dc
Rodrigo Luiz Rech <rodrigorech92@gmail.com>
2023-09-11 08:35:46 -0300

    Feat: BancoTest

--- src/test/java/com/trybe/acc/java/caixaeletronico/BancoTest.java
+++ src/test/java/com/trybe/acc/java/caixaeletronico/BancoTest.java
@@ -17,7 +17,8 @@ class BancoTest {
   @DisplayName("1 - Testa o gerador de número único para nova conta.")
   void gerarNumeroNovaContaTest() {
     Banco banco = new Banco();
-    assertEquals(10, banco.gerarNumeroNovaConta().length());
+    String conta = banco.gerarNumeroNovaConta();
+    Assertions.assertEquals(10, conta.length());
   }
 
   @Test
@@ -27,13 +28,14 @@ class BancoTest {
     String nome = "teste";
     String cpf = "123456789121";
     String senha = "123456";
-    assertEquals(PessoaCliente, banco.adicionarPessoaCliente(nome,cpf,senha).class());
-    assertEquals(1, banco.pessoasClientes.length());
+    Assertions.assertEquals(PessoaCliente.class,
+        banco.adicionarPessoaCliente(nome,cpf,senha).getClass());
+    Assertions.assertEquals(1, banco.getPessoasClientes().size());
     String nome2 = "teste1";
     String cpf2 = "123456789122";
     String senha2 = "123422";
     banco.adicionarPessoaCliente(nome2,cpf2,senha2);
-    assertEquals(2, banco.pessoasClientes.length());
+    Assertions.assertEquals(2, banco.getPessoasClientes().size());
   }
 
   @Test
@@ -44,7 +46,7 @@ class BancoTest {
     String cpf = "123456789121";
     String senha = "123456";
     PessoaCliente cliente = banco.adicionarPessoaCliente(nome,cpf,senha);
-    assertEquals(cliente, banco.pessoaClienteLogin(cpf,senha));
+    Assertions.assertEquals(cliente, banco.pessoaClienteLogin(cpf,senha));
   }
 /*
 Testar saídas console
@@ -72,8 +74,20 @@ https://cursos.alura.com.br/forum/topico-testando-saida-no-console-71576
   @Test
   @DisplayName("5 - Testa se o método sacar está funcionando corretamente.")
   void depositarTestSacarTestMostrarExtratoTest() {
-    fail("Não implementado");
-
+    Banco banco = new Banco();
+    String nomeDeposito = "vaiDepositar";
+    String cpf = "123456789121";
+    String senha = "123456";
+    PessoaCliente cliente = banco.adicionarPessoaCliente(nomeDeposito,cpf,senha);
+    Conta contaCorrente = new Conta("Corrente", cliente, banco);
+    Conta contaPoupanca = new Conta("Poupança", cliente, banco);
+    banco.depositar(cliente, 1, 5000);
+    banco.sacar(cliente,1, 1000);
+    ByteArrayOutputStream output = new ByteArrayOutputStream();
+    PrintStream ps = new PrintStream(output);
+    System.setOut(ps);
+    banco.mostrarExtrato(cliente, 1);
+    Assertions.assertTrue(output.toString().contains("4000"));
   }
 
 }
