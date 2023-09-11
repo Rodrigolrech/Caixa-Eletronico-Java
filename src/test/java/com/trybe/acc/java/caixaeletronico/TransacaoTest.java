94502bf8b7eb79ca86a7f99e80e46ec7786b3ad1
Rodrigo Luiz Rech <rodrigorech92@gmail.com>
2023-09-11 08:36:17 -0300

    Feat: TransacaoTest

--- src/test/java/com/trybe/acc/java/caixaeletronico/TransacaoTest.java
+++ src/test/java/com/trybe/acc/java/caixaeletronico/TransacaoTest.java
@@ -1,6 +1,12 @@
 package com.trybe.acc.java.caixaeletronico;
 
 import static org.junit.jupiter.api.Assertions.fail;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,7 +17,10 @@ class TransacaoTest {
   @Test
   @DisplayName("21 - Testa o método construtor da classe Transacao.")
   void construtorTest() {
-    fail("Não implementado");
+    Transacao transacao = new Transacao(200, "Saque");
+    Assertions.assertEquals(transacao.getQuantia(), 200);
+    Assertions.assertEquals(transacao.getDescricao(), "Saque");
+    Assertions.assertEquals(transacao.getInstante().length(), 19);
 
   }
 
@@ -19,22 +28,25 @@ class TransacaoTest {
   @Test
   @DisplayName("22 - Testa o método Getter do atributo quantia.")
   void getQuantiaTest() {
-    fail("Não implementado");
-
+    Transacao transacao = new Transacao(200, "Saque");
+    Assertions.assertEquals(transacao.getQuantia(), 200);
   }
 
   @Test
   @DisplayName("23 - Testa o método retornar resumo transacao.")
   void retornarResumoTransacaoTest() {
-    fail("Não implementado");
-
+    Transacao transacao = new Transacao(200, "Saque");
+    Assertions.assertTrue(transacao.retornarResumoTransacao().contains("200"));
+    Assertions.assertTrue(transacao.retornarResumoTransacao().contains("Saque"));
   }
 
   @Test
   @DisplayName("24 - Testa o método instante está gerando o instante corretamente.")
   void retornarInstanteTest() {
-    fail("Não implementado");
-
+    Transacao transacao = new Transacao(200, "Saque");
+    Assertions.assertEquals(transacao.getInstante().length(), 19);
+    Assertions.assertTrue(transacao.getInstante().contains(String.valueOf(LocalDateTime.now().getDayOfMonth())));
+    Assertions.assertTrue(transacao.getInstante().contains(String.valueOf(LocalDateTime.now().getHour())));
   }
 
 }
