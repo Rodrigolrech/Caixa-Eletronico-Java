d8a98306d4ac15457fd54e2b656db9bd6348fe53
Rodrigo Luiz Rech <rodrigorech92@gmail.com>
2023-09-11 08:37:02 -0300

    Feat: Transacao

--- src/main/java/com/trybe/acc/java/caixaeletronico/Transacao.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/Transacao.java
@@ -1,6 +1,63 @@
 package com.trybe.acc.java.caixaeletronico;
 
-public class Transacao {
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 
+public class Transacao {
+  private double quantia;
+  private String instante;
+  private String descricao;
+  private Conta conta;
+
+  public Transacao(double quantia, String descricao) {
+    this.quantia = quantia;
+    this.descricao = descricao;
+    this.instante = retornarInstante();
+  }
+
+  public double getQuantia() {
+    return quantia;
+  }
+
+  public String retornarResumoTransacao() {
+    StringBuilder builder = new StringBuilder();
+    return builder.append(instante).append(" R$").append(quantia).append(" ").append(descricao).toString();
+  }
+
+  public String retornarInstante() {
+    LocalDateTime instante = LocalDateTime.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    return instante.format(formatter);
+  }
+
+  public void setQuantia(double quantia) {
+    this.quantia = quantia;
+  }
+
+  public String getInstante() {
+    return instante;
+  }
+
+  public void setInstante(String instante) {
+    this.instante = instante;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public Conta getConta() {
+    return conta;
+  }
+
+  public void setConta(Conta conta) {
+    this.conta = conta;
+  }
 }
 
